6c753097d24c005ac8e4cf6c2e44c614be23d043
Vincenzo de Luca <61676750+Jsprofile@users.noreply.github.com>
2022-09-15 18:29:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,32 +2,56 @@ const unity = document.querySelectorAll('.unitiesArea .unity')
 const courses = document.querySelectorAll('.courses')
 const courseCard = document.querySelectorAll('.courses .swiper-slide')
 const courseDescription = document.querySelectorAll('.coursePick .courseDescription')
+const descriptionArea = document.querySelectorAll('.coursesDescriptions')
 
-unity.forEach((clickedUnity)=>{
-    clickedUnity.addEventListener('click', ()=>{
+
+unity.forEach((clickedUnity) => {
+    clickedUnity.addEventListener('click', () => {
+
+        //Add active class on unity button
+        clickedUnity.classList.add('activeUnity')
+
+        const el = [...unity].filter(e => e.classList.contains('activeUnity') && clickedUnity.innerHTML != e.innerHTML).map(elem => {
+            elem.classList.remove('activeUnity')
+        })
+
+
+        //Add active class on course swiper 
         var dataUnity = clickedUnity.dataset.unity
 
-        courses.forEach((selectedCourse)=>{
-            if(selectedCourse.dataset.unity = dataUnity){
-                selectedCourse.classList.add('active')
-            }else{
-                selectedCourse.classList.remove('active')
-            }
+        const cursel = [...courses].filter(x => x.dataset.unity == dataUnity).map(elem => {
+            elem.classList.add('active')
+        })
+        const curseDonel = [...courses].filter(x => x.dataset.unity != dataUnity).map(elem => {
+            elem.classList.remove('active')
+        })
+
+        const descriptionDisabled = [...descriptionArea].filter(x => x.dataset.unity != dataUnity).map(elem => {
+            elem.classList.add('descriptionsDisable')
+        })
+
+        const descriptionAbled = [...descriptionArea].filter(x => x.dataset.unity == dataUnity).map(elem => {
+            elem.classList.remove('descriptionsDisable')
         })
 
     })
 })
 
-courseCard.forEach((clickedCourse)=>{
-    clickedCourse.addEventListener('click', ()=>{
+
+//Add active class on course description 
+courseCard.forEach((clickedCourse) => {
+    clickedCourse.addEventListener('click', () => {
         var dataCourse = clickedCourse.dataset.course
 
-        courseDescription.forEach((clickedDescription)=>{
-            if(clickedDescription.dataset.course = dataCourse){
+        courseDescription.forEach((clickedDescription) => {
+            if (clickedDescription.dataset.course == dataCourse) {
                 clickedDescription.classList.add('activeCourse')
-            }else{
+            } else {
                 clickedDescription.classList.remove('activeCourse')
             }
         })
     })
 })
+
+
+
